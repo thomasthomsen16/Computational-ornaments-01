11303978a29807ba8b9cfaf43c8c412cd5fee66c
Thomas T <35918236+thomasthomsen16@users.noreply.github.com>
2026-05-17 14:33:48 +0200

    Create circle using arcs for initial experiment

--- src/main.js
+++ src/main.js
@@ -1,15 +1,29 @@
 import p5 from 'p5';
 
 new p5((/** @type {import('p5')} */ p) => {
+  const arcSize = 50;
+  /** @type {number[]} */
+  let sizes = [];
 
   p.setup = () => {
     p.createCanvas(p.windowWidth, p.windowHeight);
+    sizes = Array.from({ length: 4 }, () => arcSize + p.random(10, 50));
     p.background(255);
-    diameters = Array.from({ length: 5 }, () => 100 + p.random(-20, 20));
   };
 
   p.draw = () => {
-
+    let posX = p.width / 2;
+    let posY = p.height / 2;
+    p.background(255);
+    p.noFill();
+    // Bottom-right.
+    p.arc(posX, posY, sizes[0], sizes[0], 0, p.HALF_PI);
+    // Bottom-left.
+    p.arc(posX, posY, sizes[1], sizes[1], p.HALF_PI, p.PI);
+    // Top-left.
+    p.arc(posX, posY, sizes[2], sizes[2], p.PI, p.PI + p.QUARTER_PI);
+    // Top-right.
+    p.arc(posX, posY, sizes[3], sizes[3], p.PI + p.QUARTER_PI, p.TWO_PI);
   }
 
   p.windowResized = () => {
